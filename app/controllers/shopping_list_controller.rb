46bbf3f3aef7b888fdf221cda1fc31f61381d6e6
Romans S <romans.spilaks@gmail.com>
2023-09-07 16:10:13 +0300

    Implement correct price sorting using the calculated price instead of direct calculations in the view.

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,11 +1,11 @@
 class ShoppingListController < ApplicationController
   def index
     @recipe = Recipe.find(params[:recipe_id])
-    sort_column, sort_direction = determine_sorting(params[:sort], params[:direction])
+    _, sort_direction = determine_sorting(params[:sort], params[:direction])
     @recipe_foods = RecipeFood.where(recipe_id: @recipe.id)
       .includes(food: :user)
-      .order("foods.#{sort_column} #{sort_direction}")
-
+      .sort_by(&:calculated_price)
+    @recipe_foods.reverse! if sort_direction == 'desc'
     @general_shopping_list, @total = current_user.general_shopping_list(@recipe_foods)
   end
 
@@ -14,18 +14,15 @@ class ShoppingListController < ApplicationController
   def determine_sorting(sort_param, direction_param)
     column = 'name'
     direction = 'asc'
-
     case sort_param
     when 'food'
       column = 'food'
     when 'quantity'
       column = 'quantity'
     when 'price'
-      column = 'price'
+      column = 'calculated_price'
     end
-
     direction = direction_param if %w[asc desc].include?(direction_param)
-
     [column, direction]
   end
 end
--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,8 @@
 class RecipeFood < ApplicationRecord
   belongs_to :recipe
   belongs_to :food
+
+  def calculated_price
+    food.price * quantity
+  end
 end
