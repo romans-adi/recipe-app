be89ce8d66036e30c13371345cb684e85e56c001
Romans S <romans.spilaks@gmail.com>
2023-09-08 18:48:09 +0300

    Update README.md and seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    role: 'user'
+  )
+end
+
+10.times do
+  Food.create!(
+    name: Faker::Food.unique.ingredient,
+    measurement_unit: Faker::Food.metric_measurement,
+    quantity: Faker::Number.between(from: 1, to: 100),
+    price: Faker::Number.decimal(l_digits: 2)
+  )
+end
+
+10.times do
+  user = User.all.sample
+  recipe = Recipe.create!(
+    name: Faker::Food.dish,
+    preparation_time: Faker::Number.between(from: 5, to: 120),
+    cooking_time: Faker::Number.between(from: 5, to: 120),
+    description: Faker::Lorem.paragraph,
+    public: [true, false].sample,
+    user: user
+  )
+
+  3.times do
+    food = Food.all.sample
+    RecipeFood.create!(
+      quantity: Faker::Number.between(from: 1, to: 10),
+      food: food,
+      recipe: recipe
+    )
+  end
+end
